c66575b6a48aa903dca9f0da1817c6b1294bcc62
fiflokonon <fiflokonon@gmail.com>
2022-11-11 18:23:27 +0100

    cors1

--- src/Domain/Core/Middleware/CorsMiddleware.php
+++ src/Domain/Core/Middleware/CorsMiddleware.php
@@ -13,6 +13,7 @@ class CorsMiddleware
         $response = $handler->handle($request);
         return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Credentials', true)
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
     }
